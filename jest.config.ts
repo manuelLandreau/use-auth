90ab256a2214bdeaa2d42891e693964037adabd7
manuellandreau <manuel.landreau@pm.me>
2022-11-16 14:37:12 +0100

    chore: comment

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,7 @@ export default {
     clearMocks: true,
     testEnvironment: 'jsdom',
     testEnvironmentOptions: {
+        // why ? because test configuration is a mistery
         customExportConditions: [
             'node',
             'node-addons'
